47475c9ed2004a9497ed4a5498129fc22a41129b
Jake Humphrey <jake@harvestprofit.com>
2024-09-20 14:34:30 -0700

    Cleaning up the last few things

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,4 @@
 import { addons } from '@storybook/manager-api';
-import { STORY_RENDERED } from '@storybook/core-events'
 import { create } from '@storybook/theming';
 
 const theme = create({
@@ -13,32 +12,3 @@ const theme = create({
 addons.setConfig({
   theme
 });
-
-// addons.register('TitleAddon', api => {
-//   const customTitle = 'Reactstrap';
-//   let interval = null;
-//   const setTitle = () => {
-//     clearTimeout(interval);
-
-//     let storyData = null;
-//     try {
-//       storyData = api.getCurrentStoryData();
-//     } catch (e) { }
-
-//     let title;
-//     if (!storyData) {
-//       title = customTitle;
-//     } else {
-//       title = `${storyData.kind} - ${storyData.name} ⋅ ${customTitle}`
-//     }
-
-//     if (document.title !== title) {
-//       document.title = title;
-//     }
-//     interval = setTimeout(setTitle, 100);
-//   };
-//   setTitle();
-//   api.on(STORY_RENDERED, story => {
-//     setTitle();
-//   })
-// })
--- SECURITY.md
+++ SECURITY.md
@@ -8,6 +8,4 @@ Currently, only the latest version is supported.
 
 ### Security contact information
 
-To report a security vulnerability, please use the
-[Tidelift security contact](https://tidelift.com/security).
-Tidelift will coordinate the fix and disclosure.
+To report a security vulnerability, please reach out to support@harvestprofit.com
