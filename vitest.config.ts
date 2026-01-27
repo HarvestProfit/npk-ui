b8d3adf3a403b63ab47f67113775ebf6c8be2d29
Jake Humphrey <jake@harvestprofit.com>
2026-01-26 19:16:10 -0800

    Upgrading vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'vitest/config';
-import dts from "vite-plugin-dts";
-
+import dts from 'vite-plugin-dts';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-
+import { playwright } from '@vitest/browser-playwright';
 const dirname =
   typeof __dirname !== 'undefined'
     ? __dirname
@@ -15,8 +13,8 @@ const dirname =
 export default defineConfig({
   resolve: {
     alias: {
-      "@harvest-profit/npk/icons": path.resolve(__dirname, '../src/icons/'),
-      "@harvest-profit/npk": path.resolve(__dirname, '../src/')
+      '@harvest-profit/npk/icons': path.resolve(__dirname, '../src/icons/'),
+      '@harvest-profit/npk': path.resolve(__dirname, '../src/'),
     },
   },
   test: {
@@ -26,38 +24,44 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
         ],
         test: {
           name: 'storybook',
           browser: {
             enabled: true,
             headless: true,
-            provider: 'playwright',
-            instances: [{ browser: 'chromium' }],
+            provider: playwright({}),
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
           },
           setupFiles: ['.storybook/vitest.setup.ts'],
         },
-      },
+      }
     ],
   },
   build: {
     //Specifies that the output of the build will be a library.
     lib: {
-      //Defines the entry point for the library build. It resolves 
+      //Defines the entry point for the library build. It resolves
       //to src/index.ts,indicating that the library starts from this file.
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "@harvest-profit/npk",
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@harvest-profit/npk',
       //A function that generates the output file
       //name for different formats during the build
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
