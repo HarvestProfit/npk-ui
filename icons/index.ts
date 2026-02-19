763f26ea7f78f0868506013fe067c16d2ab643a0
Jake Humphrey <jake@harvestprofit.com>
2026-02-18 16:15:09 -0800

    Adding some more icons

--- icons/index.ts
+++ icons/index.ts
@@ -9,14 +9,21 @@ import SettingsFilledIcon from "./regular/SettingsFilledIcon";
 import HomeFeatureIcon from "./regular/HomeFeatureIcon";
 import HomeFeatureFilledIcon from "./regular/HomeFeatureFilledIcon";
 
+import FarmIcon from "./regular/FarmIcon";
+import FarmFilledIcon from "./regular/FarmFilledIcon";
+
+import FieldIcon from "./regular/FieldIcon";
+import FieldFilledIcon from "./regular/FieldFilledIcon";
+
 import CropPlannerFeatureIcon from "./regular/CropPlannerFeatureIcon";
 import CropPlannerFeatureFilledIcon from "./regular/CropPlannerFeatureFilledIcon";
-
 
 export function attemptFilledIconForIcon(icon: React.ComponentType<any>) {
   if (icon === CropIcon) return CropFilledIcon;
   if (icon === SettingsIcon) return SettingsFilledIcon;
   if (icon === HomeFeatureIcon) return HomeFeatureFilledIcon;
   if (icon === CropPlannerFeatureIcon) return CropPlannerFeatureFilledIcon;
+  if (icon === FarmIcon) return FarmFilledIcon;
+  if (icon === FieldIcon) return FieldFilledIcon;
   return null;
 }
